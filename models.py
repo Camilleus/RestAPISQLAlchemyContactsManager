8c3205b8c883e08333a1298874aae6e68adc97f3
CamilleusRex <camilleusrex@gmail.com>
2024-01-23 20:09:49 +0100

    added Contact class

---
+++ models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import date
+
+Base = declarative_base()
+
+class Contact(Base):
+    __tablename__ = "contacts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String, index=True)
+    last_name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    phone_number = Column(String)
+    birth_date = Column(Date)
+    additional_data = Column(String, nullable=True)
