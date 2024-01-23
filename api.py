80659d035743c392c8af2d51ca10350b28814654
CamilleusRex <camilleusrex@gmail.com>
2024-01-23 22:24:06 +0100

    added Contact Congiguration

--- api.py
+++ api.py
@@ -1,6 +1,18 @@
+# api.py
 from fastapi import FastAPI, HTTPException, Query
 from sqlalchemy.orm import Session
 from .models import Contact, Base
 from .db import get_db
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+class ContactCreateUpdate(BaseModel):
+    first_name: str
+    last_name: str
+    email: str
+    phone_number: str
+    birth_date: date
+    additional_data: str = None
+
