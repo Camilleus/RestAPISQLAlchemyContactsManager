62ee48ae6b9d9daa3106e93df475c89e15284cfe
CamilleusRex <camilleusrex@gmail.com>
2024-01-23 21:48:06 +0100

    Create db.py

---
+++ db.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from fastapi import Depends
+
+DATABASE_URL = "mysql://user:password@localhost/dbname" # Zmienię to jak tylko ją stworzę
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
