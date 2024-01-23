e7e174c73a6762029c3f824e79ead28631bc55bc
CamilleusRex <camilleusrex@gmail.com>
2024-01-23 22:02:21 +0100

    updated imports

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException, Query
 from sqlalchemy.orm import Session
-from .contact import Contact, Base
+from .models import Contact, Base
 from .db import get_db
 
 app = FastAPI()
