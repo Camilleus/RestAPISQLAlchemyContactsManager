ff0a42a98e4c38bcdfd1f2aa73fba87f85efd0c9
CamilleusRex <camilleusrex@gmail.com>
2024-01-23 22:01:48 +0100

    fixed typo

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException, Query
 from sqlalchemy.orm import Session
 from .contact import Contact, Base
-from .pg import get_db
+from .db import get_db
 
 app = FastAPI()
