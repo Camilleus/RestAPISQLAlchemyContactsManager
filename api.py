04abd08dcebcd0609bc59d6999b83f4db5e774ef
CamilleusRex <camilleusrex@gmail.com>
2024-01-24 18:31:36 +0100

    added create_contact operation

--- api.py
+++ api.py
@@ -1,10 +1,9 @@
-# api.py
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI, HTTPException, Query, Depends
 from sqlalchemy.orm import Session
-from .models import Contact, Base
+from datetime import date
+from .models import Contact
 from .db import get_db
 from pydantic import BaseModel
-from datetime import date
 
 app = FastAPI()
 
@@ -15,4 +14,15 @@ class ContactCreateUpdate(BaseModel):
     phone_number: str
     birth_date: date
     additional_data: str = None
+    
+    
+# CRUD operations
+@app.post("/contacts/", response_model=Contact)
+def create_contact(contact: ContactCreateUpdate, db: Session = Depends(get_db)):
+    db_contact = Contact(**contact.dict())
+    db.add(db_contact)
+    db.commit()
+    db.refresh(db_contact)
+    return db_contact
+
 
