048df7d262c2edfb388a9c21f450bd938a119dbf
CamilleusRex <camilleusrex@gmail.com>
2024-01-27 13:01:16 +0100

    fixed imports and changed db to mysql

--- db.py
+++ db.py
@@ -1,18 +1,24 @@
+from databases import Database
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from fastapi import Depends
-from .models import Base
 
 
-DATABASE_URL = "sqlite:///./contacts.db" 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+DATABASE_URL = "mysql+mysqlconnector://Camilleus:fghg1234@localhost/konigcontacts"
+
+
+database = Database(DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+Base = declarative_base()
 
 
 def init_db():
     Base.metadata.create_all(bind=engine)
-    
-    
+
+
 def get_db():
     db = SessionLocal()
     try:
