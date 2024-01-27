3dae3728c164c2aae6e44d176d5f5758e51798c8
CamilleusRex <camilleusrex@gmail.com>
2024-01-27 12:59:55 +0100

    fixed imports

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
-from .db import init_db
+from fastapi import FastAPI, HTTPException, Query, Depends, BackgroundTasks
+from sqlalchemy.orm import Session
+from datetime import date, timedelta
+from models import Contact
+from db import init_db, get_db
 
 
 app = FastAPI()
@@ -8,4 +11,5 @@ app = FastAPI()
 init_db()
 
 
-from .api import *
+from api import *
+
