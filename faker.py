50b92a223b8ac36785ce297075a38f4d37b64991
CamilleusRex <camilleusrex@gmail.com>
2024-01-27 13:18:42 +0100

    added async seed of contact and loop

--- faker.py
+++ faker.py
@@ -1,4 +1,7 @@
 from faker import Faker
+from datetime import datetime, timedelta
+from models import Contact
+from db import database
 
 fake = Faker()
 
@@ -12,3 +15,16 @@ def create_fake_contact():
         "additional_data": fake.text() 
     }
 
+async def seed_fake_data():
+    contacts = [create_fake_contact() for _ in range(20)]
+    
+    for contact in contacts:
+        query = Contact.__table__.insert().values(**contact)
+        await database.execute(query)
+
+if __name__ == "__main__":
+    import asyncio
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(seed_fake_data())
+    print("Fikcyjne dane zostały dodane do bazy.")
